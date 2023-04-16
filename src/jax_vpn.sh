07658db7d461972161f9e26a529dd7ad90ddd216
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 20:16:26 +0800

    Update jax_vpn.sh

--- src/jax_vpn.sh
+++ src/jax_vpn.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 api="https://api.jaxvpn.com/api/v1"
+user_agent="Dart/2.17 (dart:io)"
 
 function get_servers() {
 	curl --request GET \
 		--url "$api/vpn/servers_android" \
-		--user-agent "Dart/2.17 (dart:io)" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json"
 }
